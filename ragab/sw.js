f38ef30beb615e4c6e8e75a65a1418eff3a7c524
Sitama02 <ahmedrafat538@gmail.com>
2026-03-13 16:26:14 +0200

    Delete ragab directory

--- ragab/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // بيسمح للتطبيق يشتغل ويسحب الداتا من النت عادي جداً
-    e.respondWith(fetch(e.request));
-});
