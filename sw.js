dffb0a3cac49ee13af5bed23a5561e5b1d62cfde
Sitama02 <ahmedrafat538@gmail.com>
2026-03-15 17:23:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,45 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
+const CACHE_NAME = "ragab-league-v2";
+
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./manifest.json",
+  "./logo-192.png",
+  "./logo-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (e) => {
-    // هذا السطر يخبر جوجل كروم أن التطبيق جاهز ويعمل بكفاءة
-    e.respondWith(
-        fetch(e.request).catch(() => new Response('مرحباً، التطبيق يعمل.'))
+self.addEventListener("fetch", event => {
+
+  if (event.request.url.includes("script.google.com")) {
+    event.respondWith(
+      fetch(event.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(event.request))
     );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match("./offline.html"));
+    })
+  );
 });
